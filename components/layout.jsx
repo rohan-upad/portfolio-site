f85314e881b8963aab8abadbe00909f5502ed16a
Rohan <ru38@cornell.edu>
2021-01-09 17:46:22 -0800

    finishing touches

---
+++ components/layout.jsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import classNames from 'classnames'
+
+
+const name = 'Rohan Upadhyayula'
+export const siteTitle = 'Rohan Upadhyayula'
+
+export default function Layout({ children, home }) {
+    return (
+      <div className={styles.container}>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <header className={styles.header}>
+            <Link href = {''}><a className = {styles.homeLink}>Rohan Upadhyayula</a></Link>
+            {(1===2) && <div className = {styles.navItems}>
+                <Link href = {''}><a>About</a></Link>
+            </div>}
+        </header>
+        <main>{children}</main>
+      </div>
+    )
+  }
---
+++ pages/index.jsx
@@ -0,0 +1,68 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import styles from '../styles/Home.module.css'
+
+
+const name = 'Rohan Upadhyayula';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={styles.introContent}>
+        <div className={styles.introText}>
+          <p>Rohan is a Product Designer<br></br> from
+            San Jose, California.
+          </p>
+          <div className={styles.contactText}>
+            <h2 className = {styles.contact}>Contact</h2>
+            <a href = "https://www.linkedin.com/in/rohan-upadhyayula-b37178146/"
+            target="_blank" className = {styles.link}>LinkedIn</a>
+            <a href = "./resume.pdf"
+            target="_blank" className = {styles.link}>Resume</a>
+            <a target="_blank" href = "mailto:rohan.upad123@gmail.com" className = {styles.link}>rohan.upad123@gmail.com</a>
+          </div>
+        <div>
+
+        </div>
+        </div>
+        <img className = {styles.homeImage} src = "/images/profile.jpg"/>
+      </section>
+      <section className = {styles.portfolio}>
+        <h2 className = {styles.h2}>Portfolio</h2>
+        <div className = {styles.portfolioItems}>
+          <article className = {styles.portfolioItem}>
+            <div id = "groupMeImg"className = {styles.itemImg}></div>
+            <div className = {styles.itemContent}>
+              <p className = {styles.itemTitle}>GroupMe Announcements</p>
+              <p className = {styles.itemBody}>Taking on the infinite scroll.</p>
+              <a target="_blank" href = "https://rohanupadhyayula.medium.com/groupme-announcements-overcoming-the-infinite-scroll-6b0ac92e724b"
+              className = {styles.caseButton}>VIEW CASE STUDY</a>
+            </div>
+          </article>
+          <article className = {styles.portfolioItem}>
+            <div id = "eatlyImg"className = {styles.itemImg}></div>
+            <div className = {styles.itemContent}>
+              <p className = {styles.itemTitle}>Eatly</p>
+              <p className = {styles.itemBody}>Designing and prototyping a daily ordering platform with React.js.</p>
+              <a target="_blank" href = "https://rohanupadhyayula.medium.com/eatly-creating-a-daily-meal-ordering-tool-8860476e1b58"
+              className = {styles.caseButton}>VIEW CASE STUDY</a>
+            </div>
+          </article>
+          <article className = {styles.portfolioItem}>
+            <div id = "mingleHallImg" className = {styles.itemImg}></div>
+            <div className = {styles.itemContent}>
+              <p className = {styles.itemTitle}>Mingle Hall</p>
+              <p className = {styles.itemBody}>Meeting classmates in the pandemic age.</p>
+              <a target="_blank" href = "https://rohanupadhyayula.medium.com/mingle-hall-meeting-classmates-in-the-pandemic-age-8ca35fe13918"
+              className = {styles.caseButton}>VIEW CASE STUDY</a>
+            </div>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  )
+}
